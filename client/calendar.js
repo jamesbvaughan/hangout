64cc317c4b457fe6f9296f72deafba09dcb36800
James Vaughan <james@jamesbvaughan.com>
2015-11-19 15:35:24 -0800

    fixed some calendar problems

--- client/calendar.js
+++ client/calendar.js
@@ -9,20 +9,56 @@ Template.calendar.helpers({
 			select: function (start, end) {
 				var title = prompt("Where to go?");
 				var eventData;
-				console.log("Trying to add event " + title + " starting at " + start.format() + " and ending at " + end.format());
+				console.log("Trying to add event " + title +
+							" starting at " + start.format() +
+							" and ending at " + end.format());
 				if (title) {
 					eventData = {
 						title: title,
-						start: start,
-						end: end
+						start: start.toDate(),
+						end: end.toDate()
 					};
-					console.log("Made thing for event " + title + " starting at " + start.format() + " and ending at " + end.format());
+					console.log("Made thing for event " + title +
+								" starting at " + start.format() +
+								" and ending at " + end.format());
 					Meteor.call("addHangout", eventData);
+					console.log("CLIENT: " + Hangouts.find().fetch())
+					console.log(Hangouts.find().fetch())
 					//$("#main-calendar").fullCalendar('renderEvent', eventData, true);
 				}
 				$("#main-calendar").fullCalendar("unselect");
 			},
-			editable: true
+			editable: true,
+			timezone: "local"
 		};
+	},
+	events: function () {
+		return Hangouts.find().fetch();
+	}
+});
+
+Template.calendar.onRendered(function () {
+	updateCalendar();
+});
+
+function updateCalendar() {
+	$("#main-calendar").fullCalendar("removeEvents");
+	Hangouts.find().forEach(function (hang) {
+		$("#main-calendar").fullCalendar(
+			"renderEvent",
+			{
+				title: hang.title,
+				start: moment(hang.start),
+				end: moment(hang.end)
+			},
+			true);
+	});
+}
+
+Tracker.autorun(updateCalendar);
+
+Template.event.events({
+	"click .delete": function () {
+		Meteor.call("removeHangout", this._id);
 	}
 });
